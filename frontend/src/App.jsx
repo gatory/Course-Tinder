7cb406b2c9d8ef4c7f6bcf91386a0ffdcaa2cd33
Kuan <rueikuanwei@gmail.com>
2024-11-10 17:11:48 -0800

    MMMMMMMMMMMMMMMMMMMM

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,60 +1,6 @@
-/*import React, { useEffect, useState ,useRef} from 'react';
-import './App.css';
-import { TimePickerComponent } from "@syncfusion/ej2-react-calendars" 
-
-
-const Submit  = () => {
-  const inputRef = useRef();
-  /*var jsonData = {
-    "users": [
-        {
-            "name": "alan", 
-            "age": 23,
-            "username": "aturing"
-        },
-        {
-            "name": "john", 
-            "age": 29,
-            "username": "__john__"
-        }
-    ]
-  }
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const inputValue = inputRef.current.value;
-    fetch('http://localhost:5002/api', {
-
-      method: 'POST', 
-      mode: 'cors', 
-      
-      body: JSON.stringify({time: inputValue}) 
-
-    })
-  }
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-         <label htmlFor="get-time" style={{marginRight: '10px'}}>Time:</label>
-         <input id="get-time" type="text" ref={inputRef} />
-        </div>
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
-
-export default function App() {
-  const [message, setMessage, setInputValue, inputValue] = useState("");
-  return (
-    <div className="person-gif">
-      <div className="positioned-time">
-      <TimePickerComponent placeholder="Select a Time" />
-      </div>
-      <Submit />
-    </div>
-  );
-}*/
 import React, { useState, useRef } from 'react';
 import './App.css';
+import { TimePickerComponent } from "@syncfusion/ej2-react-calendars" 
 
 const Submit = () => {
   const inputRef = useRef();
@@ -99,8 +45,12 @@ const Submit = () => {
 };
 
 export default function App() {
+  const [message, setMessage, setInputValue, inputValue] = useState("");
   return (
-    <div>
+    <div className="person-gif">
+      <div className="positioned-time">
+      <TimePickerComponent placeholder="Select a Time" />
+      </div>
       <Submit />
     </div>
   );
