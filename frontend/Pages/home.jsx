2891b06e7c69e03e017b05f9d90ea0cf572edfcf
Kuan <rueikuanwei@gmail.com>
2024-11-10 16:47:04 -0800

    pages

---
+++ frontend/Pages/home.jsx
@@ -0,0 +1,13 @@
+// src/pages/Home.jsx
+import React from 'react';
+
+const home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to the home page of our app!</p>
+    </div>
+  );
+};
+
+export default home;
---
+++ frontend/Pages/running.jsx
@@ -0,0 +1,13 @@
+// src/pages/About.jsx
+import React from 'react';
+
+const running = () => {
+  return (
+    <div>
+      <h1>running Page</h1>
+      <p>This is the about page where we talk about our app.</p>
+    </div>
+  );
+};
+
+export default running;
